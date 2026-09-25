50f7e589a1d4badd592c0a0fd6219a4e569123a9
udayzaid <zaidquispe70364819@gmail.com>
2026-09-25 16:05:34 -0400

    refactor: move stream credentials type to shared types

---
+++ src/types/stream.ts
@@ -0,0 +1,11 @@
+export type StreamCredentials = {
+  nombre: string;
+  descripcion: string;
+  incio: string;
+  broadcastId: string;
+  watchUrl: string;
+  embeUrl: string;
+  rtmpServerUrl: string;
+  streamingKey: string;
+  estado: string;
+};
